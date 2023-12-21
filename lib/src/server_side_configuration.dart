d2064a4b9352a61fb3dd869924f51296ccb91bf9
johnson.zhong <514961926@qq.com>
2023-12-21 21:35:57 +0800

    fix: 优化server_configuration

--- lib/src/server_side_configuration.dart
+++ lib/src/server_side_configuration.dart
@@ -12,57 +12,37 @@ import 'package:lib_utils/loggers.dart';
 class ServerSideConfiguration {
   static ServerSideConfiguration instance = ServerSideConfiguration._();
 
-  bool readHistoryPermissionEnabled = true;
-
   /// 我的-数字藏品 入口 是否打开, 默认关
   bool walletIsOpen = false;
 
-  ///支付乐豆入口 是否打款, 默认关
-  bool payIsOpen = false;
-
   /// 第三方支付(微信支付宝)是否打开
-  /// 默认关 (1开启 0 关闭)
-  bool thirdPayIsOpen = false;
+  bool thirdPayIsOpen = false; // 默认关 (1开启 0 关闭)
 
   /// 是否显示第三方登录入口
-  //苹果登录 默认关
-  ValueNotifier<bool> appleLoginOpen = ValueNotifier(false);
-
-  //微信登录 默认关
-  ValueNotifier<bool> wechatLoginOpen = ValueNotifier(false);
+  ValueNotifier<bool> appleLoginOpen = ValueNotifier(false); //苹果登录 默认关
+  ValueNotifier<bool> wechatLoginOpen = ValueNotifier(false); //微信登录 默认关
 
   /// APP后台通知部分
   bool serverEnableNotiInBg = true;
   int maxNotiCountInBg = 5;
   int currentNotiCountInBg = 0;
-
-  /// -none 无信息流入口  recommend 火山推荐  hot 按fanbook热度推荐 normal 85%的正常流量数据
-  /// - 注：guild_id逻辑的优先级高于abtestBucketName，如果guild_id不等于0，那么abtestBucketName会返回空字符串
+  // -none 无信息流入口  recommend 火山推荐  hot 按fanbook热度推荐 normal 85%的正常流量数据
+  // - 注：guild_id逻辑的优先级高于abtestBucketName，如果guild_id不等于0，那么abtestBucketName会返回空字符串
   RxString abtestBucketName = ''.obs;
-
-  /// - 发现页功能用户黑名单,如果有值，则为黑名单服务器，发现页展示黑名单服务器的圈子内容
-  RxString inGuildBlack = ''.obs;
-
+  RxString inGuildBlack = ''.obs; // - 发现页功能用户黑名单,如果有值，则为黑名单服务器，发现页展示黑名单服务器的圈子内容
   double singleMaxMoney = 20000; // 发送单个红包最大金额
   int maxNum = 2000; // 拼手气红包最多分成这么多份
   int period = 24 * 60 * 60; // 默认的红包过期时间24小时，服务器配置，单位为秒
-
-  /// 配置链接黑名单
   UrlCheckEntity urlCheckEntity = UrlCheckEntity.defaultValue();
 
+  /// 配置链接黑名单
   String officialOperationBotId = "398308634552958976";
-
-  late Future<Exception?> _requestFuture;
-
-  /// 使用此 Future 确保已经完成了最新配置的加载
-  Future<void> ensureRequestDone() => _requestFuture;
-
-  ServerSideConfiguration._() {
-    init();
-  }
 
   static Completer<CommonSettingsRes>? _settingsCompleter;
   static CommonSettingsRes settings = CommonSettingsRes();
+  static Completer<bool>? _initCompleter;
+
+  ServerSideConfiguration._();
 
   static Future<CommonSettingsRes> getSettings([fetchRemote = false]) {
     if (!fetchRemote && _settingsCompleter != null) {
@@ -104,48 +84,44 @@ class ServerSideConfiguration {
     return _settingsCompleter!.future;
   }
 
-  Future<void> init() async {
+  Future<bool> init() async {
+    if (_initCompleter != null && await _initCompleter!.future) return true;
+
     // 获取本地的发现页显示配置和黑名单
-    _getDiscoverConfig();
-
-    _requestFuture = CommonApi.prerequisiteConfig(onSuccess: (config) {
-      walletIsOpen = config.walletBean;
-      payIsOpen = config.leBean;
-
-      serverEnableNotiInBg =
-          config.notificationInfo?.enableNotDisturbBgNoti ?? true;
-      maxNotiCountInBg = config.notificationInfo?.total ?? 5;
-
-      appleLoginOpen.value = config.appleLogin == 1;
-      wechatLoginOpen.value = config.wechatLogin == 1;
-      thirdPayIsOpen = config.thirdPayIsOpen == 1;
-
-      singleMaxMoney = config.redPack?.singleMaxMoney.toDouble() ?? 20000;
-      maxNum = config.redPack?.maxNum ?? 2000;
-      period = config.redPack?.period ?? 24 * 60 * 60;
-
-      officialOperationBotId =
-          config.officialOperationBotId ?? officialOperationBotId;
-
-      debugPrint(
-          'a=${appleLoginOpen.value}  w=${wechatLoginOpen.value} l=$payIsOpen');
-
-      readHistoryPermissionEnabled = config.readHistory ?? true;
-
-      urlCheckEntity = config.urlCheckEntity ?? UrlCheckEntity.defaultValue();
-    });
-
-    final exception = await _requestFuture;
-    if (exception != null) {
-      debugPrint("初始化服务端配置失败，将使用客户端默认配置。原因： $exception");
-    }
-  }
-
-  /// - 获取本地的发现页显示配置和黑名单
-  void _getDiscoverConfig() {
     abtestBucketName.value =
         SpService.instance.getString(SP.bucketName) ?? 'normal';
     inGuildBlack.value = SpService.instance.getString(SP.inGuildBlack) ?? '';
+
+    _initCompleter = Completer();
+    final exception = await CommonApi.prerequisiteConfig(
+      onSuccess: (config) {
+        walletIsOpen = config.walletBean;
+        serverEnableNotiInBg =
+            config.notificationInfo?.enableNotDisturbBgNoti ?? true;
+        maxNotiCountInBg = config.notificationInfo?.total ?? 5;
+
+        appleLoginOpen.value = config.appleLogin == 1;
+        wechatLoginOpen.value = config.wechatLogin == 1;
+        thirdPayIsOpen = config.thirdPayIsOpen == 1;
+
+        singleMaxMoney = config.redPack?.singleMaxMoney.toDouble() ?? 20000;
+        maxNum = config.redPack?.maxNum ?? 2000;
+        period = config.redPack?.period ?? 24 * 60 * 60;
+
+        officialOperationBotId =
+            config.officialOperationBotId ?? officialOperationBotId;
+
+        urlCheckEntity = config.urlCheckEntity ?? UrlCheckEntity.defaultValue();
+        _initCompleter?.complete(true);
+      },
+      onFail: (errCrode, errMsg) => _initCompleter?.complete(false),
+    ).timeout(5.seconds).catchError((e) {
+      _initCompleter?.complete(false);
+      return e;
+    });
+
+    if (exception != null) logger.info("初始化服务端配置失败，将使用客户端默认配置。原因： $exception");
+    return _initCompleter!.future;
   }
 
   /// - 更新保存到本地存储
